92f86c033d6910d7b20e1cd7dbdcaaa25c86cf8f
Andres <andrsp1@hotmail.com>
2020-05-12 16:52:17 +0200

    version 2

--- modules/module1.py
+++ modules/module1.py
@@ -1,11 +1,14 @@
 import module2 as md2
 import matplotlib.pyplot as plt
+import numpy as np
 
-(t, v, solc) = md2.time_evolution()
+(t, v, solc,E) = md2.time_evolution()
 (vx, vy, vz) = md2.components_extraction(solc) #we extract the z-component to see the slowing motion
 
-plt.plot(t, vz)
+plt.plot(t, v, label = "v")
+#plt.plot(t, vy, label = "vy")
+#plt.plot(t, vx, label = "vx")
 plt.xlabel('t/tau')
 plt.ylabel('vz/v0')
+plt.legend()
 plt.show()
-
