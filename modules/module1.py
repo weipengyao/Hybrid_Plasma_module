23321dc024e3abeb52733c978846ae345fd019e7
Andres <andrsp1@hotmail.com>
2020-05-13 22:41:45 +0200

    version 2.3 - A class Hybrid_Plasma was created in module1.py to run the code and plot the results. In module2.py, the Sherlock's approxs were implemented and a function to run the time evolution for a beam of N particles was created with the name Sherlock_func

--- modules/module1.py
+++ modules/module1.py
@@ -1,14 +1,25 @@
 import module2 as md2
 import matplotlib.pyplot as plt
-import numpy as np
 
-(t, v, solc,E) = md2.time_evolution()
-(vx, vy, vz) = md2.components_extraction(solc) #we extract the z-component to see the slowing motion
+class Hybrid_Plasma:
+    pass
+    def __init__(self, N):
+        self.N = N #particle number in the beam
 
-plt.plot(t, v, label = "v")
-#plt.plot(t, vy, label = "vy")
-#plt.plot(t, vx, label = "vx")
-plt.xlabel('t/tau')
-plt.ylabel('vz/v0')
-plt.legend()
-plt.show()
+    def run_program(self):
+        (t, vx, vy, vz, v) = md2.sherlock_func(self.N)
+        return (t, vx, vy, vz, v)
+
+    def plot_results(self):
+        (t, vx, vy, vz, v) = self.run_program()
+        plt.plot(t, v, label="v")
+        plt.xlabel('t/tau')
+        plt.ylabel('v/v0')
+        plt.legend()
+        plt.show()
+
+    def __del__(self):
+        pass
+
+plasma = Hybrid_Plasma(500)
+plasma.plot_results()
