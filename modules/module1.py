175247dc43421aa3d95e15914f032e4897e666b2
Andres <andrsp1@hotmail.com>
2020-07-01 15:31:22 +0200

    Final Version of the internship code. Two modules are added at the GitHub repo. The first one contains the Hybrid_Plasma class with two objects for the tests with 100 and 1000 particles in the particle beam with 0.01 as time step. The second module contains the main functions to solve the problem in time

--- modules/module1.py
+++ modules/module1.py
@@ -1,49 +1,56 @@
 import module2 as md2
 import matplotlib.pyplot as plt
 import os
+plt.rcParams.update({'font.size': 20})
 
-class Hybrid_Plasma:
+class Hybrid_Plasma: #Hybrid plasma class
     pass
     def __init__(self, N, delta):
         self.N = N #number of particles in the beam
         self.delta = delta #time-step
     def run_program(self):
+        #This function runs the first test for N particles with time step dt
         (t, v) = md2.sherlock_func(self.N, self.delta)
+        #The solution is saved in a CSV file
         file = open("speed_solution_{}.csv".format('data'), "w")
         for i in range(len(v)):
             file.write("{} {}".format(t[i],v[i]) + os.linesep)
         file.close()
         return (t, v)
-
     def run_programT(self):
+        # This function runs the first test for N particles with time step dt
         (t, Temp) = md2.sherlock_funcT(self.N, self.delta)
+        # The solution is saved in a CSV file
         file = open("temperature_solution_{}.csv".format('data'), "w")
         for i in range(len(Temp)):
             file.write("{} {}".format(t[i], Temp[i]) + os.linesep)
         file.close()
         return (t, Temp)
-
     def plot_results(self):
         (t, v) = self.run_program()
-        plt.plot(t, v, label="v")
-        plt.xlabel('t/tau')
-        plt.ylabel('v/v0')
+        plt.semilogy(t, v, color = 'black', linestyle = '-', label = "$N = 100$", linewidth = 0.5)
+        plt.title("$\Delta t = 0.01\\tau_{ch}$")
+        plt.xlabel('t/$\\tau_{ch}$')
+        plt.ylabel('$v/v_0$')
+        plt.legend()
         plt.legend()
         plt.show()
-
     def plot_resultsT(self):
         (t, T) = self.run_programT()
-        plt.plot(t, T, label="dt = 0.01")
-        plt.plot([0.0,8.0],[0.1,0.1], '-.')
+        plt.plot(t, T, color = 'red', linestyle = '-', label = "$\\Delta t = 0.01$", linewidth = 1)
+        plt.plot([0.0,8.0],[0.1,0.1], color = 'black', linestyle = '-.', label = "Fluid", linewidth = 1)
         plt.title("$T_i(t=0) = 10T_e$ ")
         plt.xlabel('$t/\\tau_\epsilon$')
         plt.ylabel('$T_i(t)/T_i(t=0)$')
         plt.legend()
         plt.show()
-
     def __del__(self):
         pass
 
-plasma = Hybrid_Plasma(5000, 0.01)
+#We create an object "Plasma" to run the first test
+plasma = Hybrid_Plasma(100, 0.01)
 plasma.plot_results()
-#plasma.plot_resultsT()
+#We create an second object "PlasmaT" to run the second test
+#uncomment the following two lines to plot the temperature equilibration plot and comment the first test.
+#plasmaT = Hybrid_Plasma(1000, 0.01)
+#plasmaT.plot_resultsT()
